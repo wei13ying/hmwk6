cd51082b888ee788fa174d7814aefb4a05c87529
Фадеева Ульяна Александровна ип-517 <kkk@ru>
2025-10-27 02:00:19 +0700

    Добавлен метод getCarByBrand

--- Main.java
+++ Main.java
@@ -1,5 +1,34 @@
 public class Main {
 	public static void main(String[] args){
-		//what
+		Car[] cars = {
+			new Car(1, "Hyundai", "Elantra", 2023, "White", 1559000, "123ABC"),
+            		new Car(2, "Chery", "Arrizo", 2023, "Blue", 1990000, "456DEF"),
+            		new Car(3, "Hyundai", "Solaris", 2022, "Brown",  1626000, "789GIJ")
+		};
+		Car[] hyundaiCars = getCarByBrand(cars, "Hyundai");
+		System.out.println("Машины марки " + "Hyundai" + ":");
+		for (Car car : hyundaiCars) {
+			System.out.println(car);
+		}
+	}
+	public static Car[] getCarByBrand(Car[] cars, String brand) {
+
+		int count = 0;
+		for (Car car : cars) {
+			if (car.getMarka().equalsIgnoreCase(brand)) {
+				count++;
+			}
+		}
+
+
+		Car[] result = new Car[count];
+		int index = 0;
+		for (Car car : cars) {
+			if (car.getMarka().equalsIgnoreCase(brand)) {
+				result[index] = car;
+				index++;
+			}
+		}
+		return result;
 	}
 }
